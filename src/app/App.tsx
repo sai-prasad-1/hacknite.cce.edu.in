4bea78556cbfb5b581583bfbac297542f4fed22b
AlfrinP <alfrinpoulose@gmail.com>
2024-02-23 02:03:40 +0530

    corrected a small error

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {Route, Routes } from "react-router-dom";
 import { Layout } from "@/app/Layout";
 import { Home, NoMatch } from "@/pages";
 import { About } from "@/pages/About";
@@ -7,7 +7,7 @@ import { TimeLine } from "@/pages/TimeLine";
 
 const App: FC = () => {
   return (
-    <BrowserRouter>
+    <>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index path="/home" element={<Home />} />
@@ -16,7 +16,7 @@ const App: FC = () => {
           <Route path="*" element={<NoMatch />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </>
   );
 };
 
