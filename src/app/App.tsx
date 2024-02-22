0587ae8dd4f3e44a9ccb9cd3f842012445ef29a0
AlfrinP <alfrinpoulose@gmail.com>
2024-02-23 01:57:25 +0530

    testing routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Layout } from "@/app/Layout";
 import { Home, NoMatch } from "@/pages";
 import { About } from "@/pages/About";
@@ -7,16 +7,16 @@ import { TimeLine } from "@/pages/TimeLine";
 
 const App: FC = () => {
   return (
-    <>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/timeline" element={<TimeLine/>} />
+          <Route index path="/home" element={<Home />} />
+          <Route path="/timeline" element={<TimeLine />} />
           <Route path="/about" element={<About />} />
           <Route path="*" element={<NoMatch />} />
         </Route>
       </Routes>
-    </>
+    </BrowserRouter>
   );
 };
 
