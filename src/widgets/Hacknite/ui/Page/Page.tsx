64c725a6137d113536fb472cf6622a00dfdf04a1
Mick Suraj <micksuraj999@gmail.com>
2024-02-22 20:35:13 +0530

    test

--- src/widgets/Hacknite/ui/Page/Page.tsx
+++ src/widgets/Hacknite/ui/Page/Page.tsx
@@ -1,13 +1,14 @@
 import { CustomFont } from "@/widgets/CustomFont";
 import { FC } from "react";
 
-
 const Hacknite: FC = () => {
     return (
         <>
-            <div className="flex mb-6 sm:mb-12 lg:mb-0 gap-5 bg-center items-center justify-center flex-col p-10">
-                <img src="images/hacknight.png" alt="" className="w-72 object-contain" />
-                <CustomFont description >Welcome to HackNite, a thrilling 24-hour coding competition hosted by the vibrant code community of developers at Christ College of Engineering. Embrace the challenge as you dive into a whirlwind of innovation, collaboration, and coding prowess, all in the pursuit of crafting groundbreaking solutions.</CustomFont>
+            <div className="flex flex-col items-center p-5 md:p-10 sm:p-15">
+                <img src="images/hacknight.png" alt="" className="w-full max-w-md object-contain mb-6" />
+                <CustomFont description>
+                    Welcome to HackNite, a thrilling 24-hour coding competition hosted by the vibrant code community of developers at Christ College of Engineering. Embrace the challenge as you dive into a whirlwind of innovation, collaboration, and coding prowess, all in the pursuit of crafting groundbreaking solutions.
+                </CustomFont>
             </div>
         </>
     );
